7aa41e130d493f3a4b9946d673d2b0f2a06726b0
Andrew <atburke1985@gmail.com>
2020-07-18 13:49:21 -0500

    Serve static files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,36 @@
-from flask import Flask, render_template
-  
+from flask import Flask, render_template, abort, redirect, url_for, send_file
+
 app = Flask(__name__)
 
-  
-@app.route("/") 
-def home_view(): 
-        return render_template("index.html")
+# I would 100% appreciate a better solution for this
+served_files = frozenset((
+    "asset-manifest.json",
+    "index.html",
+    "favicon.ico",
+    "manifest.json",
+    "robots.txt",
+    "service-worker.js",
+))
+
+served_binary_files = frozenset((
+    "logo192.png",
+    "logo512.png",
+))
+
+@app.route("/<file>")
+def serve_root_file(file):
+    if file in served_files:
+        return render_template(file)
+
+    if file in served_binary_files:
+        return send_file(f"templates/{file}")
+
+    abort(404)
+
+@app.errorhandler(404)
+def not_found(e):
+    return redirect(url_for("index"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
---
+++ app/templates/precache-manifest.6abebc6aee0a7cdbd0063f4052a781cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef71ae643fa45f036504493b4d9d1b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f436ab34e69451f7b0",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0e2fc3f8ff65330e5163",
+    "url": "/static/js/2.e6839bd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6839bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f436ab34e69451f7b0",
+    "url": "/static/js/main.0e0c2015.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
