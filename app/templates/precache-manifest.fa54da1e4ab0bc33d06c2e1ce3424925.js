c00216f24ed6584f2f227f8596f9805f506ec5b2
Andrew <atburke1985@gmail.com>
2020-07-26 11:43:21 -0500

    Add missing frontend files

---
+++ app/templates/precache-manifest.fa54da1e4ab0bc33d06c2e1ce3424925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2924827517fd57e74103a8fd51d5b4fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d58f9c762815226ce09",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7187f86e72891e64f32a",
+    "url": "/static/js/2.f257c4ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f257c4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d58f9c762815226ce09",
+    "url": "/static/js/main.871b28a0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
